dd4e7c9b45a0cfb1d6077138c61328fb9b0bedda
andre <a.ekani@gmail.com>
2024-11-18 09:36:39 +0100

    Update packages

--- app.config.ts
+++ app.config.ts
@@ -2,11 +2,37 @@ import { defineConfig } from '@tanstack/start/config';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import svgr from 'vite-plugin-svgr';
+import vite from 'vite';
+
+// export default defineConfig({
+//   deployment: {
+//     preset: 'node-server',
+//     static: false,
+//     prerender: false,
+//   },
+//   tsr: {
+//     appDirectory: './src',
+//     routesDirectory: './src/routes',
+//     generatedRouteTree: './src/routeTree.gen.ts',
+//   },
+//   routers: {
+//     client: { entry: './src/client.tsx' },
+//     ssr: { entry: './src/ssr.tsx' },
+//   },
+//   vite: {
+//     plugins: (...args) => [
+//       svgr({ include: '**/*.svg' }),
+//       vanillaExtractPlugin(),
+//       tsConfigPaths({
+//         projects: ['./tsconfig.json'],
+//       }),
+//     ],
+//   },
+// });
 
 export default defineConfig({
   deployment: {
     preset: 'node-server',
-    static: false,
   },
   tsr: {
     appDirectory: './src',
@@ -18,12 +44,16 @@ export default defineConfig({
     ssr: { entry: './src/ssr.tsx' },
   },
   vite: {
-    plugins: () => [
-      svgr({ include: '**/*.svg' }),
-      vanillaExtractPlugin(),
-      tsConfigPaths({
-        projects: ['./tsconfig.json'],
-      }),
-    ],
+    ssr: {
+      noExternal: ['@djeka07/ui', /\.css$/],
+    },
+    plugins: () =>
+      [
+        svgr({ include: '**/*.svg' }),
+        vanillaExtractPlugin(),
+        tsConfigPaths({
+          projects: ['./tsconfig.json'],
+        }),
+      ] as vite.Plugin<any>[],
   },
 });
